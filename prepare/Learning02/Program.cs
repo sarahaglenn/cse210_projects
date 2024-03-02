be0181953f74b5698b8058c2711c34fa1707f7aa
Sarah Glenn <sarahadaline@gmail.com>
2024-03-02 21:51:34 +0100

    writing Job and Resume classes

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2005;
+        job1._endYear = 2020;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Receptionist";
+        job2._company = "Dunder Mifflin";
+        job2._startYear = 2003;
+        job2._endYear = 2005;
+
+        Resume resume1 = new Resume();
+        resume1._name = "Sarah Glenn";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.DisplayResume();
     }
 }
